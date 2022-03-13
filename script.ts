2fcf200fa9b9800aeaefa6a4ea85f4d304fd789b
Ida Darmochwał <ida.mdart@gmail.com>
2022-03-13 21:21:52 +0100

    Declare types

--- script.ts
+++ script.ts
@@ -1,24 +1,24 @@
-function showMyName(name) {
+function showMyName(name: string): void {
   console.log(name)
 }
 
 showMyName('John')
 
-function square(a) {
+function square(a: number): number {
   return a * a
 }
 
 square(10)
 
-function sum(title, ...numbers) {
+function sum(title: string, ...numbers: number[]): string {
   return title + ' = ' + numbers.reduce((sum, num) => sum + num, 0)
 }
 
 sum('Numbers', 1, 6, 10)
 
-const executeFunc = func => {
-  const title = 'Lorem Ipsum'
+const executeFunc = (func: (arg1: string, arg3: number, arg2: string[]) => void) => {
+  const title: string = 'Lorem Ipsum'
   func(title, 24, ['Sport', 'Movies'])
 }
 
-executeFunc((title, age, hobbies) => { return true })
+executeFunc((title: string, age: number, hobbies: string[]) => { return true })
