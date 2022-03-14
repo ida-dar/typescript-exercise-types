d049ad31bba9957f885faaa7850f1f6d84484089
Ida Darmochwał <ida.mdart@gmail.com>
2022-03-14 21:04:14 +0100

    Change type declaration of executeFunc

--- script.ts
+++ script.ts
@@ -16,7 +16,7 @@ function sum(title: string, ...numbers: number[]): string {
 
 sum('Numbers', 1, 6, 10)
 
-const executeFunc = (func: (arg1: string, arg3: number, arg2: string[]) => void) => {
+const executeFunc = (func: (arg1: string, arg3: number, arg2: string[]) => boolean) => {
   const title: string = 'Lorem Ipsum'
   func(title, 24, ['Sport', 'Movies'])
 }
